301b4eeaa64e6366c257e8056645b348bfb78944
Brian Su <briansu2004@hotmail.com>
2022-05-19 13:26:42 -0400

    easy

---
+++ Problem Solving/0001_Simple-Array-Sum/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+I
+src/main/scala/stdin1.txt,a\b\abc6e3858518a346198cae9fee0aedf2a252ac6f
+H
+src/main/scala/stdin.txt,7\3\734662b521c82344e7c23d79b3a5161fc3945721
+M
+src/main/scala/Solution.scala,4\0\40046f5a679a68bf4c6cb344057cb07045905824
---
+++ Problem Solving/0001_Simple-Array-Sum/src/main/scala/Solution.scala
@@ -0,0 +1,56 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.concurrent._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.reflect._
+import scala.sys._
+import scala.util.matching._
+
+object Result {
+
+  /*
+   * Complete the 'simpleArraySum' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts INTEGER_ARRAY ar as parameter.
+   */
+
+  def simpleArraySum(ar: Array[Int]): Int = {
+    // Write your code here
+    var sum: Int = 0
+
+    for (i <- ar.indices) {
+      sum = sum + ar(i)
+    }
+
+    sum
+  }
+
+}
+
+object Solution {
+  def main(args: Array[String]) {
+    val printWriter = new PrintWriter(System.out) //sys.env("OUTPUT_PATH"))
+
+    val arCount = StdIn.readLine.trim.toInt
+
+    val ar = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+    val result = Result.simpleArraySum(ar)
+
+    printWriter.println(result)
+
+    printWriter.close()
+  }
+}
