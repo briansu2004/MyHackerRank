8538972a13d141adfe53ea137f8e6b77149b67cc
Brian Su <briansu2004@hotmail.com>
2022-06-11 17:42:15 -0400

    BigInt

--- Scala/Algorithms/0001_String-Similarity/src/main/scala/Solution.scala
+++ Scala/Algorithms/0001_String-Similarity/src/main/scala/Solution.scala
@@ -9,21 +9,21 @@ object Result {
    * The function accepts STRING s as parameter.
    */
 
-  def stringSimilarity(s: String): Int = {
+  def stringSimilarity(s: String): BigInt = {
     // Write your code here
-    var n = s.length
+    var n = BigInt(s.length)
 
-    val Z: Array[Int] = Array.fill[Int](s.length)(0)
+    val Z: Array[BigInt] = Array.ofDim[BigInt](s.length)
     getZarr(s, s.length, Z)
 
     for (i <- 1 until s.length) {
-      val ns = s.slice(i, s.length)
       n = n + Z(i)
     }
+
     n
   }
 
-  def getZarr(s: String, n: Int, Z: Array[Int]): Unit = {
+  def getZarr(s: String, n: Int, Z: Array[BigInt]): Unit = {
     /// [L,R] make a window which matches with prefix of s
     var L = 0
     var R = 0
@@ -66,6 +66,64 @@ object Result {
     }
   }
 
+//  def stringSimilarityInt(s: String): Int = {
+//    // Write your code here
+//    var n = s.length
+//
+//    //val Z: Array[Int] = Array.fill[Int](s.length)(0)
+//    val Z: Array[Int] = Array.ofDim[Int](s.length)
+//    getZarrInt(s, s.length, Z)
+//
+//    for (i <- 1 until s.length) {
+//      n = n + Z(i)
+//    }
+//
+//    n
+//  }
+//
+//  def getZarrInt(s: String, n: Int, Z: Array[Int]): Unit = {
+//    /// [L,R] make a window which matches with prefix of s
+//    var L = 0
+//    var R = 0
+//
+//    for (i <- 1 until n) {
+//      // if i>R nothing matches so we will calculate Z[i] using naive way.
+//      if (i > R) {
+//        L = i
+//        R = i
+//
+//        // R-L = 0 in starting, so it will start checking from 0'th index.
+//        // e.g. for "ababab" and i = 1, the value of R remains 0 and Z[i] becomes 0.
+//        //  For string "aaaaaa" and i = 1, Z[i] and R become 5.
+//        while (R < n && s.charAt(R - L) == s.charAt(R)) {
+//          R = R + 1
+//        }
+//
+//        Z(i) = R - L
+//        R = R - 1
+//      } else {
+//        // k = i-L so k corresponds to number which matches in [L,R] interval.
+//        val k = i - L
+//
+//        // if Z[k] is less than remaining interval, then Z[i] will be equal to Z[k].
+//        // e.g. str = "ababab", i = 3, R = 5 and L = 2
+//        if (Z(k) < R - i + 1) {
+//          Z(i) = Z(k)
+//        }
+//        // For example str = "aaaaaa" and i = 2, R is 5, L is 0
+//        else {
+//          // else start from R and check manually
+//          L = i
+//          while (R < n && s.charAt(R - L) == s.charAt(R)) {
+//            R = R + 1
+//          }
+//          Z(i) = R - L
+//          R = R - 1
+//        }
+//      }
+//    }
+//  }
+
   //  def stringSimilarityN2(s: String): Int = {
   //    // Write your code here
   //    var n = s.length
@@ -98,7 +156,7 @@ object Solution {
 
     val t = StdIn.readLine.trim.toInt
 
-    for (tItr <- 1 to t) {
+    for (_ <- 1 to t) {
       val s = StdIn.readLine
 
       val result = Result.stringSimilarity(s)
