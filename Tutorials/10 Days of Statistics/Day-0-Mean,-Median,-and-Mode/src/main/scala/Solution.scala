51274bb31b6ff7eb8a2583b94452274f0aefb302
Brian Su <briansu2004@hotmail.com>
2022-06-18 11:35:37 -0400

    mode

--- Tutorials/10 Days of Statistics/Day-0-Mean,-Median,-and-Mode/src/main/scala/Solution.scala
+++ Tutorials/10 Days of Statistics/Day-0-Mean,-Median,-and-Mode/src/main/scala/Solution.scala
@@ -2,24 +2,37 @@ import java.io._
 import scala.io._
 
 object Result {
-  def getMeanMedianMode(arr: Array[BigInt]): Array[BigDecimal] = {
+  def getMeanMedianMode(arr: Array[Int]): Array[BigDecimal] = {
     val len = arr.length
+
+    /* mean */
     val mean = BigDecimal(arr.sum) / len
+
+    /* median */
     var median: BigDecimal = 0
     //val sortedArr: Array[BigInt] = arr.sortBy(_)(Ordering[BigInt])
-    val sortedArr: Array[BigInt] = arr.sorted
+    val sortedArr: Array[Int] = arr.sorted
     if (len % 2 == 1) {
       median = BigDecimal(sortedArr((len + 1) / 2))
     } else {
       median = (BigDecimal(sortedArr(len / 2 - 1)) + BigDecimal(sortedArr(len / 2))) / 2
     }
 
-    val mode: BigDecimal = 0
+    /* mode */
+    var mode: BigDecimal = 0
 
-    var modeArr: Array[BigInt] = Array.fill(0)(arr.max + 1)
+    var m = arr.max + 1
+    var count: Array[Long] = Array.fill[Long](m)(arr.max + 1)
     for (i <- arr.indices) {
-      //modeArr(arr(i)) = modeArr(arr(i)) + 1
-      println(arr(i))
+      count(arr(i)) = count(arr(i)) + 1
+    }
+
+    var k = count(0)
+    for (i <- 1 until m) {
+      if (count(i) > k) {
+        k = count(i)
+        mode = i
+      }
     }
 
     Array(mean, median, mode)
@@ -33,7 +46,7 @@ object Solution {
 */
     val printWriter = new PrintWriter(System.out);
     val count = StdIn.readLine.trim.toInt
-    val arr = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim).map(BigInt(_))
+    val arr = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
 
     val res = Result.getMeanMedianMode(arr)
     printWriter.println(res.mkString("\n"))
