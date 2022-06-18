ae37faf6e0b047091b1ee3bb6c2e2187442406cc
Brian Su <briansu2004@hotmail.com>
2022-06-18 12:20:55 -0400

    day0

---
+++ Tutorials/10 Days of Statistics/Day-0-Weighted-Mean/src/main/scala/Solution.scala
@@ -0,0 +1,41 @@
+import scala.io._
+import scala.math._
+
+object Result {
+
+  /*
+   * Complete the 'weightedMean' function below.
+   *
+   * The function accepts following parameters:
+   *  1. INTEGER_ARRAY X
+   *  2. INTEGER_ARRAY W
+   */
+
+  def weightedMean(X: Array[Int], W: Array[Int]): BigDecimal = {
+    // Write your code here
+    var score: BigDecimal = 0
+    var total: BigDecimal = 0
+
+    for (i <- X.indices) {
+      score = score + X(i) * W(i)
+      total = total + W(i)
+    }
+
+    (score / total).setScale(1)
+  }
+
+}
+
+object Solution {
+  def main(args: Array[String]) {
+    val n = StdIn.readLine.trim.toInt
+
+    val vals = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+    val weights = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+    val res = Result.weightedMean(vals, weights)
+
+    println(res)
+  }
+}
