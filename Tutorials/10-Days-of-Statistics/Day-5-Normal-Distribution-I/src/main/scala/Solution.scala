f79846cbba017af8094162e7d0d5d08bb1256f98
Brian Su <briansu2004@hotmail.com>
2022-06-23 15:22:53 -0400

    day5

--- Tutorials/10-Days-of-Statistics/Day-5-Normal-Distribution-I/src/main/scala/Solution.scala
+++ Tutorials/10-Days-of-Statistics/Day-5-Normal-Distribution-I/src/main/scala/Solution.scala
@@ -1,20 +1,51 @@
-object Solution {
-  val factorial: LazyList[Int] = {
-    def f: LazyList[Int] = {
-      1.toInt #:: f.zipWithIndex.map { n => n._1 * (n._2 + 2) }
-    }
-    1 #:: f
-  }
+import scala.io._
+import scala.math._
 
-  def getProbability(mean: Double, n: Int): Double = {
-    scala.math.pow(mean, n) * scala.math.exp(-mean) / factorial(n)
+class Erf {
+  // constants
+  val a1: Double = 0.254829592
+  val a2: Double = -0.284496736
+  val a3: Double = 1.421413741
+  val a4: Double = -1.453152027
+  val a5: Double = 1.061405429
+  val p: Double = 0.3275911
+
+  def erf(x: Double): Double = {
+    // Save the sign of x
+    val sign = if (x < 0) -1 else 1
+    val absx = math.abs(x)
+
+    // A&S formula 7.1.26, rational approximation of error function
+    val t = 1.0 / (1.0 + p * absx)
+    val y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * math.exp(-x * x)
+    sign * y
+  }
+}
+
+object Solution extends Erf {
+  def getProbability(mean: Double, std: Double, n: Double): Double = {
+    0.5 * (1 + erf((n - mean) / (std * pow(2, 0.5))))
   }
 
   def main(args: Array[String]): Unit = {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
 */
-    val mean = scala.io.StdIn.readDouble()
-    val n = scala.io.StdIn.readInt()
-    println(f"${getProbability(mean, n)}%.3f")
+    val mean_std = StdIn.readLine.trim.split(" ")
+    val mean = mean_std(0).toDouble
+    val std = mean_std(1).toDouble
+
+    var s = StdIn.readLine
+    while (s != null) {
+      val d = s.trim.split(" ")
+      if (d.length == 1) {
+        println(f"${getProbability(mean, std, d(0).toDouble)}%.3f")
+      } else if (d.length == 2) {
+        println(f"${getProbability(mean, std, d(1).toDouble) - getProbability(mean, std, d(0).toDouble)}%.3f")
+      } else {
+        println(s"Please provide 1 or 2 values")
+      }
+
+      s = StdIn.readLine
+    }
   }
 }
