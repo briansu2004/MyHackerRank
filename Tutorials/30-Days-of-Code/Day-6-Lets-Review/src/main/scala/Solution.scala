3aec4f1714e9a63d51e2b361cce7a3850c41966f
Brian Su <briansu2004@hotmail.com>
2022-06-23 09:51:24 -0400

    day6

--- Tutorials/30-Days-of-Code/Day-6-Lets-Review/src/main/scala/Solution.scala
+++ Tutorials/30-Days-of-Code/Day-6-Lets-Review/src/main/scala/Solution.scala
@@ -1,19 +1,42 @@
 object Solution {
   def parseString(s: String): String = {
+    val odd: Array[Char] = Array.ofDim[Char](s.length)
+    val even: Array[Char] = Array.ofDim[Char](s.length)
 
-    s
+    for (i <- 0 until s.length by 2) {
+      even(i) = s(i)
+      if (i < s.length - 1) {
+        odd(i + 1) = s(i + 1)
+      }
+    }
+
+    even.filter(_.isLetter).mkString("") + " " + odd.filter(_.isLetter).mkString("")
   }
 
-  def main(args: Array[String]) {
+//  def parseString1(s: String): String = {
+//    var odd: List[Char] = List()
+//    var even: List[Char] = List()
+//
+//    s.zipWithIndex foreach {
+//      case (c, index) =>
+//        if (index % 2 == 1) {
+//          odd = odd ::: c :: Nil
+//        } else {
+//          even = even ::: c :: Nil
+//        }
+//    }
+//
+//    even.mkString("") + " " + odd.mkString("")
+//  }
+
+  def main(args: Array[String]): Unit = {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
 */
-    val n = 2;
-    val s1 = "Hacker"
-    val s2 = "Rank"
-
-
-    println(parseString(s1))
-    println(parseString(s2))
+    val T = scala.io.StdIn.readInt()
+    for (_ <- 1 to T) {
+      val str = scala.io.StdIn.readLine()
+      println(parseString(str))
+    }
   }
 }
 
