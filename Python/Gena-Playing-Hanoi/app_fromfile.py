149f830a2b056646d58274814841b0201ac063c1
Brian Su <briansu2004@hotmail.com>
2022-06-23 20:11:30 -0400

    yes

--- Python/Gena-Playing-Hanoi/app_fromfile.py
+++ Python/Gena-Playing-Hanoi/app_fromfile.py
@@ -3,6 +3,7 @@ import os
 import random
 import re
 import sys
+from collections import deque
 
 #
 # Complete the 'hanoi' function below.
@@ -12,63 +13,74 @@ import sys
 #
 
 
-from collections import deque
-
-
-def legal_moves(x):
-    for i in range(len(x)):
-        if x[i]:
-            for j in range(len(x)):
-                if not x[j] or x[i][-1] < x[j][-1]:
-                    yield (i, j)
-
-
-def is_goal(x):
-    return all([len(x[i]) == 0 for i in range(1, len(x))])
-
-
-def bfs(x):
-    def tuplify(z):
-        return tuple(tuple(t) for t in z)
-
-    def do_move(g, m):
-        y = [list(t) for t in g]
-        y[m[1]].append(y[m[0]].pop())
-        # WLOG sort 2nd-4th stacks by order of largest disk
-        y[1:4] = sorted(y[1:4], key=lambda t: t[-1] if t else 0)
-        return tuplify(y)
-
-    visited = set()
-
-    start = (tuplify(x), 0)
-
-    q = deque([start])
-    visited.add(start)
-
-    while q:
-        node, depth = q.popleft()
-
-        if is_goal(node):
-            return depth
-
-        for move in legal_moves(node):
-            child = do_move(node, move)
-            if child not in visited:
-                visited.add(child)
-                q.append((child, depth+1))
-
-
-def hanoi(posts):
+def hanoi(tower):
     # Write your code here
-    return bfs(posts)
+    l = len(tower)
+    rangel = range(l)   # it'll help optimisation, the solution
+    # fails time to time
+    steps = 0
+    state = 0   # state is binary of  |discN|discN-1|...|disc2|disc1|
+    # where each discK = rod on which that disc is located
+    win = 0     # win is state where each discK == 00b
+
+    def move(rod, disc):
+        nonlocal state
+        disc *= 2            # each disc is represented as 2 bits
+        state &= ~(3 << disc)  # 3 = all rods -> remove all rods for that disc
+        state |= rod << disc   # write down rod
+
+    def get_rod_top(rod):
+        st = state
+        for i in rangel:
+            r = 3 & st        # get topmost disc rod
+            if r == rod:
+                return i
+            st >>= 2        # move to the next disc
+        return l            # we can return anything, but l is better because we get
+        # less comparison later
+
+    for i in rangel:        # fill in the state
+        move(tower[i] - 1, i)
+
+    if state == win:        # bfs won't work if we are already in a right state
+        return 0
+
+    # to solve the case, we will use breadth-first search
+    bfs = deque()       # the list of unexplored states
+    bfs.append(state)   # root state atop
+    depth = dict()      # depth measure
+    depth[state] = 0    # depth of starting state is 0
+    range4 = range(4)   # just a bit of optimisation will help
+    while True:
+        cstate = state = bfs.popleft()  # get the next unexplored state
+        steps = depth[state]            # get it's depth
+
+        # get all the tops of the rods to move them
+        d = [get_rod_top(r) for r in range4]
+        for fro in range4:    # try to move from every rod
+            if d[fro] == l:     # if rod is empty, skip
+                continue
+            for to in range4:  # iterate over every other rod to put onto
+                if d[to] > d[fro]:  # if the disc is bigger on the rod or rod is empty
+                    state = cstate  # restore state to state of exploration
+                    move(to, d[fro])  # move the rod
+                    if state == win:  # if achieved finish state, return immediately
+                        return steps + 1
+                        # if state hadn't been found yet
+                    if state not in depth:
+                        # set state depth
+                        depth[state] = steps + 1
+                        # add into exploration queue
+                        bfs.append(state)
+    return depth[0]
 
 
 if __name__ == '__main__':
     with open('stdin.txt') as f:
-
         n = int(f.readline().strip())
 
         loc = list(map(int, f.readline().rstrip().split()))
 
         res = hanoi(loc)
+
         print(res)
