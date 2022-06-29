8628863a0df24205947ca1d24cd37972a1eb3f57
Brian Su <briansu2004@hotmail.com>
2022-06-29 17:16:10 -0400

    yes

---
+++ Java/Day-12-Inheritance/Day12.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/*
+Heraldo Memelli 8135627
+2
+100 80
+ */
+class Day12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.next();
+        String lastName = scanner.next();
+        int id = scanner.nextInt();
+        int numScores = scanner.nextInt();
+        Queue<Integer> scores = new LinkedList<>();
+        for (int i = 0; i < numScores; i++) {
+            scores.add(scanner.nextInt());
+        }
+        double averageScore = scores.stream().mapToInt(Integer::intValue).sum() / numScores;
+        char grade;
+        if (averageScore >= 90) {
+            grade = 'O';
+        } else if (averageScore >= 80) {
+            grade = 'E';
+        } else if (averageScore >= 70) {
+            grade = 'A';
+        } else if (averageScore >= 55) {
+            grade = 'P';
+        } else if (averageScore >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'T';
+        }
+
+        System.out.printf("Name: %s, %s\n", lastName, firstName);
+        System.out.printf("ID: %s\n", id);
+        System.out.printf("Grade: %s", grade);
+    }
+}
